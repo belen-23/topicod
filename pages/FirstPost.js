4a22b0d25c315eab5f35b2749fce5df6c1404e28
belen-23 <152044772+belen-23@users.noreply.github.com>
2024-10-15 20:50:10 -0600

    Add files via upload

---
+++ pages/FirstPost.js
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+
+export default function FirstPost() {
+   return (
+      <>
+         <h1>My First Post</h1>
+         <h2>
+            <Link href="/">
+               Home
+            </Link>
+         </h2>
+         <br/>
+         <img src="../images/fig2020.jpg" alt="Logo" />
+      </>	  
+   )
+}
